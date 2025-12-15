8b26c967e1b3d8c347fbf4fa4a107c4c65f07eef
Gyanendra Maurya <gmaurya973@gmail.com>
2025-12-15 23:22:26 +0530

    fix alignment for desktop mode

--- convertlab-frontend/convert-lab/src/app/app.routes.ts
+++ convertlab-frontend/convert-lab/src/app/app.routes.ts
@@ -3,16 +3,16 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'extract-pdf',
+    redirectTo: 'merge-pdf',
     pathMatch: 'full'
-  },
-  {
-    path: 'extract-pdf',
-    loadComponent: () => import('./components/pdf/extract-page/extract-page.component').then(m => m.ExtractPageComponent)
   },
   {
     path: 'merge-pdf',
     loadComponent: () => import('./components/pdf/merge-pdf/merge-pdf.component').then(m => m.MergePdfComponent)
+  },
+  {
+    path: 'extract-pdf',
+    loadComponent: () => import('./components/pdf/extract-page/extract-page.component').then(m => m.ExtractPageComponent)
   }
 
 ];

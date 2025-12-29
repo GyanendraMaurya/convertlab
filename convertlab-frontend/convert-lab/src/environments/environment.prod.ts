5b8bbf7cc216f020972c989a379140edfef53f2b
Gyanendra Maurya <gmaurya973@gmail.com>
2025-12-29 19:03:12 +0530

    update version script

--- convertlab-frontend/convert-lab/src/environments/environment.prod.ts
+++ convertlab-frontend/convert-lab/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: '/api',
-  version: '1.1.2',
-  buildTime: '2025-12-25T11:22:37.993Z'  // Will be replaced during build
+  version: '1.0.4',
+  buildTime: '2025-12-29T12:55:13.520Z'  // Will be replaced during build
 };
--- convertlab-frontend/convert-lab/src/environments/environment.ts
+++ convertlab-frontend/convert-lab/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080/api',
-  version: '1.1.2',
-  buildTime: '2025-12-25T11:22:37.993Z'  // Will be replaced during build
+  version: '1.0.4',
+  buildTime: '2025-12-29T12:55:13.520Z'  // Will be replaced during build
 };

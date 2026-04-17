479148be4720c2dc420bf3d73dcb47ee63562d31
Gyanendra Maurya <gyanendramaurya98@gmail.com>
2026-04-17 21:37:40 +0530

    update sitemap and environment build time; enhance chat panel styles

--- convertlab-frontend/convert-lab/src/environments/environment.prod.ts
+++ convertlab-frontend/convert-lab/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiUrl: '/api',
   version: '1.1.4',
-  buildTime: '2026-01-06T17:24:42.066Z',  // Will be replaced during build
+  buildTime: '2026-04-08T15:19:00.019Z',  // Will be replaced during build
   googleClientId: '267522460988-gl0qihftspipbumibnc5k5upmn7brs0b.apps.googleusercontent.com'
 };
